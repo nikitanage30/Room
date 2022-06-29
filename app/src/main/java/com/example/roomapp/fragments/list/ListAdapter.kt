33f000b345b4aad5303bc40d841a1aa9b02764b3
nikitanage30 <nikitan@coreflexsolutions.com>
2022-06-29 15:21:18 +0530

    my first commit

---
+++ app/src/main/java/com/example/roomapp/fragments/list/ListAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.roomapp.fragments.list
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomapp.R
+import com.example.roomapp.model.User
+import kotlinx.android.synthetic.main.custom_row.view.*
+
+class ListAdapter : RecyclerView.Adapter<ListAdapter.ViewHolder>() {
+
+    private var userList = emptyList<User>()
+    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.custom_row,parent,false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentItem = userList[position]
+        holder.itemView.id_txt.text = currentItem.id.toString()
+        holder.itemView.firstName_txt.text = currentItem.firstName
+        holder.itemView.lastName_txt.text = currentItem.lastName
+        holder.itemView.age_txt.text = currentItem.age.toString()
+
+        holder.itemView.rowLayout.setOnClickListener {
+            val action = ListFragmentDirections.actionListFragmentToUpdateFragment(currentItem)
+            holder.itemView.findNavController().navigate(action)
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setData(user : List<User>) {
+        this.userList = user
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int {
+       return userList.size
+    }
+}
---
+++ app/src/main/java/com/example/roomapp/fragments/update/UpdateFragment.kt
@@ -0,0 +1,95 @@
+package com.example.roomapp.fragments.update
+
+import android.app.AlertDialog
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
+import android.view.*
+import androidx.fragment.app.Fragment
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.roomapp.R
+import com.example.roomapp.model.User
+import com.example.roomapp.viewmodel.UserViewModel
+import kotlinx.android.synthetic.main.fragment_update.*
+import kotlinx.android.synthetic.main.fragment_update.view.*
+
+
+class UpdateFragment : Fragment() {
+
+    private val args by navArgs<UpdateFragmentArgs>()
+    private lateinit var mUserViewModel : UserViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_update, container, false)
+
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+        view.update_firstName_et.setText(args.currentUser.firstName)
+        view.update_LastName_et.setText(args.currentUser.lastName)
+        view.update_age_txt.setText(args.currentUser.age.toString())
+
+        view.update_btn.setOnClickListener {
+
+            updateItem()
+        }
+
+        //Add menu
+        setHasOptionsMenu(true)
+
+        return view
+    }
+
+    private fun updateItem() {
+        val firstName = update_firstName_et.text.toString()
+        val lastName = update_LastName_et.text.toString()
+        val age = Integer.parseInt(update_age_txt.text.toString())
+
+        if (inputCheck(firstName, lastName, update_age_txt.text)) {
+            val updatedUser = User(args.currentUser.id,firstName,lastName,age)
+            mUserViewModel.updateUser(updatedUser)
+            Toast.makeText(requireContext(),"Updated Successfully!",Toast.LENGTH_SHORT).show()
+
+            //Navigate back
+            findNavController().navigate(R.id.action_updateFragment_to_listFragment)
+        }
+        else {
+            Toast.makeText(requireContext(),"Please fill out all fields",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+        private fun inputCheck(firstName: String, lastName: String, age: Editable): Boolean {
+            return !(TextUtils.isEmpty(firstName) && TextUtils.isEmpty(lastName) && age.isEmpty())
+        }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.delete_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.menu_delete) {
+            deleteUser()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun deleteUser() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes") { _, _ ->
+        mUserViewModel.deleteUser(args.currentUser)
+            Toast.makeText(requireContext(),
+                "Successfully removed! : ${args.currentUser.firstName}",
+                Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_updateFragment_to_listFragment)
+        }
+        builder.setNegativeButton("No") { _, _ -> }
+        builder.setTitle("Delete ${args.currentUser.firstName}?")
+        builder.setMessage("Are you sure want to delete ${args.currentUser.firstName}")
+        builder.create().show()
+    }
+}
